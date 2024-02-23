d4f6e919d4ac90d6afb92d056d20a86d9ecd21e2
erik sax <erikjsax@gmail.com>
2024-02-23 12:50:55 -0500

    working on rebal stuff

--- sim7.js
+++ sim7.js
@@ -60,12 +60,12 @@ csv()
   let performance = [];
   let monthly = [];
   let treasuries = [];
- 
+  let balanceHist = []; 
   for(const entry of sorted){
     if(!entry.yield) {
       continue;
     }
-
+    console.log(entry.date);
     let sp_value = (sp_shares * entry.price); 
       let dividend = 0;
       dividend = ((sp_value * entry.yield) / 1200);
@@ -76,10 +76,18 @@ csv()
         cash = cash + coupon;
       }
 
-      if(treasuries.length > 120){
-        let matured = treasuries.shift();
-        cash = cash + matured.amount;
+      let filteredTreasuries = []
+      for(let note of treasuries){
+        if(note.amount === 0){
+          continue;
+        }
+        if(note.maturity <= new Date(entry.date)){
+          cash = cash + note.amount;
+          continue;
+        }
+        filteredTreasuries.push(note);
       }
+      treasuries = filteredTreasuries; 
 
       // Management expenses
       nav = cash + sp_value;
@@ -96,7 +104,7 @@ csv()
       }
     
 
-    let bondInvestment = cash * 1;
+    let bondInvestment = cash * .2;
     if(bondInvestment > 0){
       let yield = treas_yields[entry.date];
       if(yield){
@@ -117,6 +125,10 @@ csv()
 
     let treas_value = 0;
     for(const note of treasuries) {
+      if(note.amount === 0){
+        note.marketPrice = 0;
+        continue;
+      }
       let rate = treas_yields[entry.date] / 100;
       let maturity = note.maturity;
       let settlement = new Date(entry.date);
@@ -125,11 +137,70 @@ csv()
       let frequency = 2;
       let convention = '30U/360',
       b = bondCalculator({settlement, maturity, rate: yield, redemption, frequency, convention});
-      treas_value = treas_value + b.price(rate);
+      note.marketPrice = b.price(rate);
+      treas_value = treas_value + note.marketPrice;
     }
 
     sp_value = (sp_shares * entry.price); 
     nav = cash + sp_value + treas_value;
+
+    let sp_percent = (sp_value / nav) * 100;
+    balanceHist.push([entry.date.toDateString(), sp_percent]);
+    
+    if(sp_percent > 82){
+      console.log("Too high: " + sp_percent);
+
+      let rebalance = ((sp_percent - 80) / 2) / 100;
+      rebalance = rebalance * nav;
+      console.log("Rebalance: " + rebalance);
+      let sellShares = Math.floor(rebalance / entry.price);
+      cash = cash + (sellShares * entry.price);
+      sp_shares = sp_shares - sellShares;
+
+      let bondInvestment = cash;
+      if(bondInvestment > 0){
+        let yield = treas_yields[entry.date];
+        if(yield){
+          let maturity = new Date(entry.date);
+          maturity.setFullYear(maturity.getFullYear() + 10);
+          treasuries.push({date: entry.date, amount: bondInvestment, yield, maturity});
+          cash = cash - bondInvestment;
+        }
+      }
+    }
+    else if(sp_percent < 78){
+      console.log("Too Low: " + sp_percent);
+      let rebalance = ((80 - sp_percent) / 2) / 100;
+      rebalance = rebalance * nav;
+      console.log("Rebalance: " + rebalance);
+      for(const note of treasuries) {
+        if(note.marketPrice >= rebalance){
+          let reduction = note.amount * (rebalance / note.marketPrice);
+          console.log("Reduction: " + reduction);
+          note.amount = note.amount - reduction;
+          console.log("Note amount rem: " + note.amount);
+          cash = cash + rebalance;
+          break;
+        }
+        else{
+          note.amount = 0;
+          cash = cash + note.marketPrice;
+          rebalance = rebalance - note.marketPrice;
+          console.log("Redeemed full note for " + note.marketPrice);
+        }
+      } 
+      if(entry.price < cash){
+        let shares = Math.floor(cash / entry.price);
+        sp_shares += shares;
+        cash = cash - (shares * entry.price);
+        //console.log('Rebal S&P %s, %i', entry.date.toISOString(), shares);
+      }
+   
+    }
+    else{
+      console.log("Goldilox: " + sp_percent);
+    }
+    
     performance.push([entry.date.toDateString(), nav])
     let months = performance.length;
     if(months > 241){
@@ -160,5 +231,6 @@ csv()
   console.log("Final nav: " + nav.toFixed(2));
   fs.writeFileSync('./monthly.json', JSON.stringify(monthly, null, 2));
   fs.writeFileSync('./perf.json', JSON.stringify(performance, null, 2));
+  fs.writeFileSync('./bal.json', JSON.stringify(balanceHist, null, 2));
 });
 
